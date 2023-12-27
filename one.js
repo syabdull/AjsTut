ca3ba68d3845c831a70f54b55de3e83f9a9b8402
syabdull <syab5050@gmail.com>
2023-12-27 11:17:00 +0530

    2d array traversal

---
+++ one.js
@@ -0,0 +1,12 @@
+let arr = [
+  [1, 2],
+  [100, 200],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+console.log("-----");
+console.log(arr[1][1]);
