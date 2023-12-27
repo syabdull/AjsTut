b7b281203ccc2686b7f8037c53b4a0af2d03d705
syabdull <syab5050@gmail.com>
2023-12-27 12:20:10 +0530

    objects

--- one.js
+++ one.js
@@ -1,10 +1,31 @@
-let arr = [[1, 2][(1, 2)][(1, 2)]];
+let obj = {
+  name: "javeed",
+  age: 23,
+  a: [1, 2, 3],
+  b: function () {
+    console.log("hi  htere");
+  },
+  address: {
+    street: 12,
+    hno: "mycolor",
+  },
+};
+// Access trough dot notation
+console.log(obj.name);
+//Access trough bracket notation
+console.log(obj.a);
+console.log(obj["a"]);
+console.log(obj["b"]);
+console.log(obj.b());
+console.log(obj.address.street);
+console.log(obj.address["hno"]);
+// computed propery
+obj.javeed = "good boy";
+obj.javeed = "bad boy";
+obj.age = 32;
+delete obj.age;
+console.log(obj);
 
-for (let i = 0; i < 3; i++) {
-  i0123b;
-  arr.push([]);
-  for (let j = 0; j < 2; j++) {
-    j012b012b012b;
-    arr[i][j] = j + 1;
-  }
-}
+let { name: sai, a: myva } = obj;
+
+console.log(sai, myva);
