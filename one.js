9c352bc9df824872cb9192466c7326618c77de55
syabdull <syab5050@gmail.com>
2023-12-27 11:30:58 +0530

    in single line

--- one.js
+++ one.js
@@ -2,9 +2,14 @@ let arr = [
   [12, 34, 56, 7],
   [6, 7, 9],
 ];
-
+let finalVar = "[";
 for (let i = 0; i < arr.length; i++) {
+  let myvar = "[";
   for (let j = 0; j < arr[i].length; j++) {
-    console.log(arr[i][j]);
+    myvar = myvar + arr[i][j] + ",";
   }
+  myvar = myvar + "]";
+  finalVar = finalVar + myvar;
 }
+finalVar = finalVar + "]";
+console.log(finalVar);
